9f88c471175336a5e4da67ce754171c1b8264485
MathiasThoria <mathiassoriapiro@gmail.com>
2022-12-20 23:02:42 -0300

    Add styles of knobs. Add shake it. Add animation of shakeit

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
-printPixels();
+let x=0;
+let y=0;
 
+x = 64;
+y = 48;
+printPixels(x,y);
+animations();
 function printPixels(x=0,y=0){
     
     
@@ -8,17 +13,31 @@ function printPixels(x=0,y=0){
     
     const canvas = document.querySelector('.conteinerCanvas');
 
-    x = 64;
-    y = 48;
+   
     for (let pixels = 0; pixels < (x * y); pixels++){
         const divsito = document.createElement('div');
         canvas.appendChild(divsito);
         divsito.classList.add('divsito');
-        divsito.style.width = (640/x)-2 + "px";
-        divsito.style.height = (480/y)-2 + "px";
-        console.log(pixels);
+        divsito.style.width = (640/x) + "px";
+        divsito.style.height = (480/y) + "px";
+        divsito.addEventListener('mouseover',(e) =>
+         divsito.style.backgroundColor="grey");
+        /*console.log(pixels);*/
         
     };
 
 
+}
+function animations(){
+    const shakeIt = document.querySelector('.shakeIt');
+    console.log(shakeIt);
+    shakeIt.addEventListener('mouseover', () => {
+        shakeIt.classList.add('animationExpandHover');        
+        console.log(shakeIt);
+    });
+    shakeIt.addEventListener('mouseout', () => {
+        shakeIt.classList.remove('animationExpandHover');
+        console.log(shakeIt);
+    });
+    
 }
